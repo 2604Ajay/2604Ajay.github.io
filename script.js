390db05db196ea763c82e2a4a078cb5cb11b964b
2604Ajay <ajaysakpal0809@gmail.com>
2022-09-04 15:01:03 +0530

    Data Updated

---
+++ script.js
@@ -0,0 +1,40 @@
+
+let ab=document.getElementById("aboutme");
+let ho=document.getElementById("myhobby");
+let ed=document.getElementById("myeductaion");
+let ca=document.getElementById("mycareer");
+let hd=document.getElementById("hobbydata");
+
+
+function about(){
+    console.log("about clicked");
+   ab.style.display="block";
+   
+   ho.style.display="none";
+   ed.style.display="none";
+   ca.style.display="none";
+
+}
+function hobby(){
+    console.log("hobby clicked");
+    hd.innerText.replace
+    ab.style.display="none";
+    ho.style.display="block";
+    ed.style.display="none";
+    ca.style.display="none";
+
+}
+function education(){
+    console.log("education clicked");
+    ab.style.display="none";
+    ho.style.display="none";
+    ed.style.display="block";
+    ca.style.display="none";
+}
+function career(){
+    console.log("career clicked");
+    ab.style.display="none";
+    ho.style.display="none";
+    ed.style.display="none";
+    ca.style.display="block";
+}
